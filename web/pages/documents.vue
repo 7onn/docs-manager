d98fb0764b266db32b15b20935c88d8eb32c8854
Tom M G <devbytom@gmail.com>
2024-03-24 18:57:11 +0100

    fix(web): Avoid double json parse

--- web/pages/documents.vue
+++ web/pages/documents.vue
@@ -9,10 +9,8 @@
       <h2>Documents</h2>
       <ul style="display: flex; flex-wrap: wrap;">
         <li v-for="document, index in documents" :key="index" style="margin-right: 10px;">
-          <!-- <p>{{ document.uuid  }}</p> -->
           <p><a :href="document.url" target="_blank">{{ document.name }}</a></p>
           <embed :src="document.url" width="200px" height="300px" />
-          <!-- <embed src="http://localhost:7777/doc?uuid=5df63112-4050-419c-9913-ff13a6e4b80e" width="300px" height="600px" /> -->
         </li>
       </ul>
     </div>
@@ -50,17 +48,16 @@ export default {
           },
         })
         .then(data => {
-          console.log(data)
-          this.documents = JSON.parse(data)
+          if (data) {
+            for (const doc of data) {
+              doc.url = useRuntimeConfig().public.apiUrl + "doc?uuid=" + doc.uuid
+              this.documents.push(doc)
+            }
+          }
         })
         .catch(error => {
           console.error('Error pulling docs:', error);
         })
-
-      // this.documents = [
-      //   { id: "1", name: "resume1", url: "https://www.7onn.dev./resume.pdf" },
-      //   { id: "2", name: "resume2", url: "https://www.7onn.dev./resume.pdf" },
-      // ]
     }
   }
 };
